f9eb8b7be91f9544dd0233bad06259cbf2cf1c78
ViliamJusko-laptop <viliam180898@gmail.com>
2022-10-18 19:45:53 +0200

    starting some woooodoooo

--- logic/data_extractor.py
+++ logic/data_extractor.py
@@ -5,7 +5,7 @@ import re
 def extract(regex_airplane_name):
     print("extracting")
 
-    '([0-9]+)-(.+)_([0-9]+)-([0-9]+)(.html$)|([0-9]+)-((.+)_*)([0-9]+)(.html$)'
+    # '([0-9]+)-(.+)_([0-9]+)-([0-9]+)(.html$)|([0-9]+)-((.+)_*)([0-9]+)(.html$)'
 
     rootdir = "data/"
     # regex = re.compile(f'(.*zip$)|(.*rar$)|(.*r01$)')
@@ -15,5 +15,17 @@ def extract(regex_airplane_name):
         for file in files:
             if regex.match(file):
                 print(file)
-                opened_file = open(f'data/{file}', "r", encoding="UTF-8")
-                print(opened_file.read())
+                seareched_file = open(f'data/{file}', "r", encoding="UTF-8")
+
+                opened_file = seareched_file.read()
+
+                max_speed_regex = re.compile('<li><b>Maximum speed:<\/b> (.*)<\/li>')
+                title_regex = re.compile('<title>(.+)<\/title>')
+
+                title = re.findall(title_regex, opened_file)
+                max_speed = re.findall(max_speed_regex, opened_file)
+                print(title, max_speed)
+
+
+def compare_airplanes(airplane_1, airplane_2):
+    pass
